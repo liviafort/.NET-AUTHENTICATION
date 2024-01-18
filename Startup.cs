8f196086c77d193e8d14daf98fb7220d6070cd18
liviafort <livia.fortunato@academico.ifpb.edu.br>
2024-01-18 12:04:57 -0300

    Adicionando autorização e autenticação no Configure

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 //Arquivo crucial para a configuração inicial da aplicação e para definir como ela lida com solicitações e serviços
 using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API
@@ -12,7 +13,6 @@ namespace API
         {
             Configuration = configuration;
         }
-
 
         /*
         Essencialmente, esta parte do código está configurando a aplicação para aceitar tokens JWT com determinadas características (emitido por um emissor específico, para uma audiência específica, etc.) e registrando os serviços necessários para lidar com solicitações HTTP usando o padrão MVC.
@@ -27,7 +27,22 @@ namespace API
                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SXkSqsKyNUyvGbnHs7ke2NCq8zQzNLW7mPmHbnZZ")),
                 ClockSkew = TimeSpan.Zero // remove delay of token when expire
             };
-            services.AddControllers();
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.TokenValidationParameters = TokenValidationParameters;
+                });
+            services.AddAuthorization(cfg =>
+                {
+                    cfg.AddPolicy("Admin", policy => policy.RequireClaim("type", "Admin"));
+                    cfg.AddPolicy("Agent", policy => policy.RequireClaim("type", "Agent"));
+                    cfg.AddPolicy("ClearanceLevel1", policy => policy.RequireClaim("ClearanceLevel", "1", "2"));
+                    cfg.AddPolicy("ClearanceLevel2", policy => policy.RequireClaim("ClearanceLevel", "2"));
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -39,10 +54,15 @@ namespace API
 
             app.UseRouting();
 
+            app.UseAuthorization();
+            
+            app.UseAuthentication();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
