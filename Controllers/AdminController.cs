3628ceb9a28ac9c6333d8db7844e5235ea207d52
liviafort <livia.fortunato@academico.ifpb.edu.br>
2024-01-18 12:28:13 -0300

    Adicionar classe User ---> Criação de rota para Login no controller do admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,8 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Controllers
 {
@@ -7,5 +11,37 @@ namespace API.Controllers
     public class AdminController : ControllerBase
     {
         public AdminController() { }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Login([FromBody] User User)
+        {
+            // TODO: Authenticate Admin with Database
+            // If not authenticate return 401 Unauthorized
+            // Else continue with below flow
+
+            var Claims = new List<Claim>
+                    {
+                        new Claim("type", "Admin"),
+                    };
+
+            var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SXkSqsKyNUyvGbnHs7ke2NCq8zQzNLW7mPmHbnZZ"));
+
+            var Token = new JwtSecurityToken(
+                "https://fbi-demo.com",
+                "https://fbi-demo.com",
+                Claims,
+                expires: DateTime.Now.AddDays(30.0),
+                signingCredentials: new SigningCredentials(Key, SecurityAlgorithms.HmacSha256)
+            );
+
+            return new OkObjectResult(new JwtSecurityTokenHandler().WriteToken(Token));
+        }
     }
+
+    public class User{
+        string username = "";
+        string password = "";
+    }
+
 }
